e618bee332f72ab7067f043d60f91835cc08b5b9
Aldo Ibarra <aibarra@ciatransmaritima.com>
2024-10-21 18:10:00 -0600

    Presion de teclas y ajustes de jugabilidad

--- pages/play/play.js
+++ pages/play/play.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 //Solo estilos de página
 $(document).ready(function() {
@@ -9,16 +11,29 @@ $(document).ready(function() {
     $('.pause-container').hide();
 
  //Validacion de teclas
-$(document).keypress(function(e) {
-    const tecla = String.fromCharCode(e.which).toLowerCase(); 
+    $(document).keypress(function(e) {
+        const tecla = String.fromCharCode(e.which).toLowerCase(); 
 
-    const specificTopo = MoleModel.find(topo => topo.userData.key === tecla);
+        const specificTopo = MoleModel.find(topo => topo.userData.key === tecla);
 
-    if (specificTopo) {
-        console.log(`¡Topo golpeado con la tecla ${tecla.toUpperCase()}!`);
-        hitTopo(specificTopo); 
-    }
-})
+        if (specificTopo) {
+            console.log(`¡Topo golpeado con la tecla ${tecla.toUpperCase()}!`);
+            hitTopo(specificTopo); 
+        }
+    })
+
+    let isKeyPressed = false;
+    $(document).keydown(function(event){
+        if(!isKeyPressed){
+            pressDown(event.key);
+            isKeyPressed = true;
+        }
+        console.log(event.key);
+    });
+    $(document).keyup(function(event) {
+        isKeyPressed = false;
+        pressUp(event.key);
+    });
 });
 
 //Golpe al topo
@@ -99,10 +114,10 @@ const topPositionsAndKeys = [
     { position: { x: -4.7, y: -1, z: -1 }, key: 'a' }, // A
     { position: { x: -1, y: -1, z: -1 }, key: 'd' }, // D
     { position: { x: -3, y: -1, z: 1 }, key: 's' }, // S
-    { position: { x: 3, y: -1, z: -3 }, key: '6' }, // 6
-    { position: { x: 4.7, y: -1, z: -1 }, key: '2' }, // 2
-    { position: { x: 1, y: -1, z: -1 }, key: '8' }, // 8
-    { position: { x: 3, y: -1, z: 1 }, key: '4' }    // 4
+    { position: { x: 3, y: -1, z: -3 }, key: '8' }, // 8
+    { position: { x: 4.7, y: -1, z: -1 }, key: '6' }, // 6
+    { position: { x: 1, y: -1, z: -1 }, key: '4' }, // 4
+    { position: { x: 3, y: -1, z: 1 }, key: '2' }    // 2
 ];
 
 /*---Configuración de escena---*/
@@ -124,7 +139,7 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+//scene.add(box);
 
 /*---Carga de modelo---*/
 let MoleModel = [];
@@ -188,12 +203,116 @@ const loader = new THREE.TextureLoader();
 const textureBackground = loader.load('../resources/space3.png');
 scene.background = textureBackground;
 
+/* Fuente */
+const fontLoader = new FontLoader();
+
+function generateText(text) {
+    return new Promise((resolve, reject) => {
+      fontLoader.load('../resources/gentilis_bold.typeface.json', function(font) {
+        const textGeometry = new TextGeometry(text, {
+          font: font,
+          size: 0.8,          // Tamaño del texto
+          height: 0.1,      // Profundidad del texto (grosor)
+          curveSegments: 12, // Segmentos de curva para suavidad
+          bevelEnabled: true,   // Habilitar bisel para efecto 3D
+          bevelThickness: 0.03, // Grosor del bisel
+          bevelSize: 0.02,      // Tamaño del bisel
+          bevelOffset: 0,       // Desplazamiento del bisel
+          bevelSegments: 5      // Segmentos del bisel
+        });
+        const textMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        resolve(textMesh);
+      }, undefined, function(error) {
+        reject(error);
+      });
+    });
+  }
+var W = new THREE.Mesh();
+var A = new THREE.Mesh();
+var S = new THREE.Mesh();
+var D = new THREE.Mesh();
+var EIGHT = new THREE.Mesh();
+var TWO = new THREE.Mesh();
+var SIX = new THREE.Mesh();
+var FOUR = new THREE.Mesh();
+generateText('W').then(textMesh => {
+    textMesh.position.set(-3.3, 0, -1.5);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    W = textMesh;
+    scene.add(W);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('S').then(textMesh => {
+    textMesh.position.set(-3.3, 0, 0.5);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    S = textMesh;
+    scene.add(S);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('A').then(textMesh => {
+    textMesh.position.set(-4.1, 0, -0.6);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    A = textMesh;
+    scene.add(A);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('D').then(textMesh => {
+    textMesh.position.set(-2.2, 0, -0.6);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    D = textMesh;
+    scene.add(D);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('4').then(textMesh => {
+    textMesh.position.set(1.5, 0, -0.6);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    FOUR = textMesh;
+    scene.add(FOUR);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('6').then(textMesh => {
+    textMesh.position.set(3.6, 0, -0.6);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    SIX = textMesh;
+    scene.add(SIX);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('8').then(textMesh => {
+    textMesh.position.set(2.5, 0, -1.5);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    EIGHT = textMesh;
+    scene.add(EIGHT);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
+
+generateText('2').then(textMesh => {
+    textMesh.position.set(2.7, 0, 0.6);
+    textMesh.rotation.x = -0.5 * Math.PI;
+    TWO = textMesh;
+    scene.add(TWO);
+    }).catch(error => {
+        console.error('Error al cargar la fuente:', error);
+});
 
 animate();
 
 function animate(){
     
-    pl.position.set(positions.x, positions.y + 2, positions.z);
+    pl.position.set(positions.x, positions.y + 3.8, positions.z);
     box.position.set(positions.x, positions.y, positions.z);
 
     requestAnimationFrame(animate);
@@ -207,4 +326,66 @@ function animate(){
     });
 
     renderer.render( scene, camera );
+}
+
+function pressDown(tecla){
+    switch (tecla) {
+        case 'w' || 'W':
+            W.position.set(-3.3, -0.1, -1.5);
+            break;
+        case 'a' || 'A':
+            A.position.set(-4.1, -0.1, -0.6);
+            break;
+        case 's'  || 'S':
+            S.position.set(-3.3, -0.1, 0.5);
+            break;
+        case 'd' || 'D':
+            D.position.set(-2.2, -0.1, -0.6);
+            break;
+        case '8':
+            EIGHT.position.set(2.5, -0.1, -1.5);
+            break;
+        case '4':
+            FOUR.position.set(1.5, -0.1, -0.6);
+            break;
+        case '6':
+            SIX.position.set(3.6, -0.1, -0.6);
+            break;
+        case '2':
+            TWO.position.set(2.7, -0.1, 0.6);
+            break;
+        default:
+          break;
+    }
+}
+
+function pressUp(tecla){
+    switch (tecla) {
+        case 'w' || 'W':
+            W.position.set(-3.3, 0, -1.5);
+            break;
+        case 'a' || 'A':
+            A.position.set(-4.1, 0, -0.6);
+            break;
+        case 's'  || 'S':
+            S.position.set(-3.3, 0, 0.5);
+            break;
+        case 'd' || 'D':
+            D.position.set(-2.2, 0, -0.6);
+            break;
+        case '8':
+            EIGHT.position.set(2.5, 0, -1.5);
+            break;
+        case '4':
+            FOUR.position.set(1.5, 0, -0.6);
+            break;
+        case '6':
+            SIX.position.set(3.6, 0, -0.6);
+            break;
+        case '2':
+            TWO.position.set(2.7, 0, 0.6);
+            break;
+        default:
+          break;
+    }
 }
