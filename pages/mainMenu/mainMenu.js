c6edb4526438e8e0517b77bcac0ab73c7815a8d0
Fettuccinni <aldomaximino1@hotmail.com>
2024-11-25 04:24:30 -0600

    Validacion de facebook desactivada temporalmente

--- pages/mainMenu/mainMenu.js
+++ pages/mainMenu/mainMenu.js
@@ -52,7 +52,8 @@ window.fbAsyncInit = function() {
 
 function onLogin(){
     if($('#user').val() != null && $('#user').val() != ''){
-      fbLogIn();
+      login();
+      //fbLogIn();
     }else{
       alert("Ingresa tu usuario");
     }
